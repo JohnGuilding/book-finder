90a9c22a5944a53eda0bbdf27f19a3855382f04e
John Guilding <jwguilding@gmail.com>
2020-11-17 13:05:05 +0000

    search fuctionality done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { CollectionComponent } from './collection/collection.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+const routes: Routes = [
+  {path: '', component: SearchComponent},
+  {path: 'collection', component: CollectionComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    CollectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+interface GoogleBooksResponse {
+  items: any[];
+  kind: string;
+  totalItems: number;
+}
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+  searchText: string;
+  faHeart = faHeart;
+  books: any[] = [];
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  handleSearch() {
+    const searchedWords = this.searchText;
+    console.log('working');
+    let url = `https://www.googleapis.com/books/v1/volumes?q=inauthor=${searchedWords}`
+    return this.http.get(url).toPromise().then((response: GoogleBooksResponse) => {
+      console.log(response.items);
+      // this.books.push(response.items);
+      this.books = response.items.map(book => {
+        book = book.volumeInfo;
+        return book;
+      });
+      console.log(this.books);
+
+    });
+  }
+
+  handleNavigate() {
+    this.router.navigate(['/collection']);
+  }
+
+}
